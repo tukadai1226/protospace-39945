0891fec7763f443f1b1793ceabd561715e2d5513
tukadai1226 <tukadai1226@icloud.com>
2023-10-12 17:19:49 +0900

    ログアウト状態でプロトタイプ詳細ページに遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,10 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only:[:new, :edit, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!,  except: [:index, :show]
+  before_action :move_to_index, only: [:edit,:update, :destroy]
   
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -53,10 +53,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless current_user.id == @prototype.user_id
-      redirect_to action: :index
-    end
+    redirect_to root_path unless current_user == @prototype.user
   end  
 
 end
